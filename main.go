681fa0f727d2769f691a5626479022cb6b4c991c
Stanley-0ps <ochaistanley7@egmail.com>
2026-06-29 14:39:37 +0100

    text counter(URL variables & methods)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -11,6 +12,7 @@ func main() {
 	http.HandleFunc("/ping", pingHandler)
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/goodbye", goodbyeHandler)
+	http.HandleFunc("/count", countHandler)
 
 	fmt.Println("Server is running on http://localhost:8080")
 
@@ -45,4 +47,23 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "GoodBye!")
+}
+
+func countHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprint(w, "Send a POST request with text to count words")
+
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body", http.StatusInternalServerError)
+			return
+		}
+		length := len(body)
+		fmt.Fprintf(w, "%d", length)
+
+	default:
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
