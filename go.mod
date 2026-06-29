17ac645de17116875e7a45918251d7db6c67ca44
Stanley-0ps <ochaistanley7@gmail.com>
2026-06-29 10:43:32 +0100

    created a basic ping pong server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ascii-web-exercises
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// registers the handler for the /ping route
+	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/goodbye", goodbyeHandler)
+
+	fmt.Println("Server is running on http://localhost:8080")
+
+	// start the HTTP Server
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// pingHandler handles requests to the /ping route
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "pong 🏓")
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
+
+func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "GoodBye!")
+}
